c4f47792e97494fc33a5152bbace34d435d2a435
Lonfar <Lonfar.Code@outlook.com>
2015-11-24 14:05:10 +0100

    Revert "更新"
    This reverts commit ec3a8df88cb00c0285748dda47cfeaafef531d60.

--- WindowsForms/index.cs
+++ WindowsForms/index.cs
@@ -42,13 +42,7 @@ namespace WindowsForms
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                this.ExportInventory(folderBrowserDialog1.SelectedPath + @"material inventry report " + StartDateTime.Value.ToString("yyyy-MM-dd") + " to " + EndDateTime.Value.ToString("yyyy-MM-dd") + ".xls");
-            }
-        }
-        ///导出库存表
-        private void ExportInventory(string SavePath)
-        {
-            //初始化数据库操作类
+                //初始化数据库操作类
                 ODBC odbc = new ODBC();
                 DataTable dt;
                 string sql = @"select * from [INVENTORY]"; //where [Receive Date]>='" + StartDateTime.Value.ToString("yyyy-MM-dd") + "' and [Receive Date]<='" + EndDateTime.Value.ToString("yyyy-MM-dd") + "'";
@@ -57,7 +51,6 @@ namespace WindowsForms
                 //读取库存表
                 dt = odbc.ExecuteDataTable(sql);
                 excel.ImportDataTable("ALL", 8, dt);
-                //此处应该循环添加
                 dt = odbc.ExecuteDataTable(sql + " where [Depart Desc]='Production Dept.'");
                 excel.ImportDataTable("Production Dept.", 8, dt);
                 dt = odbc.ExecuteDataTable(sql + " where [Depart Desc]='Field Operation Dept.' or [Depart Desc]='Base camp'");
@@ -77,23 +70,8 @@ namespace WindowsForms
                 dt = odbc.ExecuteDataTable(sql + " where [Depart Desc]='P&L Dept.'");
                 excel.ImportDataTable("P&L Dept.", 8, dt);
                 ////保存库存
-                excel.SaveExcel(SavePath);
-        }
-        private void ExportReceiving(string SavePath)
-        {
-             
-        }
-        private void ExportIssue(string SavePath)
-        {
-            
-        }
-        private void ExportTransferBack (string SavePath)
-        {
-            
-        }
-        private void ExportTransferWellToWell  (string SavePath)
-        {
-            
+                excel.SaveExcel(folderBrowserDialog1.SelectedPath + @"material inventry report " + StartDateTime.Value.ToString("yyyy-MM-dd") + " to " + EndDateTime.Value.ToString("yyyy-MM-dd") + ".xls");
+            }
         }
     }
 }
